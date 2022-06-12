f54db11bfbb1983438c3512e3ffede81415c0945
smowu <smowu@users.noreply.github.com>
2022-06-13 07:18:25 +0800

    Initial commit

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+  $user = "root";
+  $pass = "";
+  $host = "localhost";
+  $dbname = "trustex";
+  $port = "3307";
+
+  $connect = mysqli_connect($host,$user,$pass,$dbname,$port);
+
+  if (!isset($connect)) {
+    echo("Error: Failed to connect to the database!"); 
+  }
+?>
