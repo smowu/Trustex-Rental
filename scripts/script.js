52840cb7fdfd140c2bca6620883acef5b80680fe
smowu <smowu@users.noreply.github.com>
2022-06-16 06:41:05 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,3 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-/*
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-*/
