b560e044b860b927118d0e3859dfb4e08b422707
smowu <smowu@users.noreply.github.com>
2022-06-15 18:35:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
   include("dbconnect.php"); 
 
   if (isset($connect)) {
+
     echo file_get_contents("html/header.html");
     echo file_get_contents("html/body.html");
     echo file_get_contents("html/footer.html");
     mysqli_close($connect);
+    
   }
 ?>
